d7878be053bb898254f2e0270579f944a3063a88
BitStarCoin <319293894+BitStarCoin@users.noreply.github.com>
2026-08-21 17:06:44 +0200

    Update public project URLs

--- src/common/license_info.cpp
+++ src/common/license_info.cpp
@@ -25,7 +25,7 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/BitStarCoin/bitstar-core>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR).translated + " ") + "\n" +
            "\n" +
